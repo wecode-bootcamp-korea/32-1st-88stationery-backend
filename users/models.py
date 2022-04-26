44c2e1e80015659fa91cc4c1652b932952b3a163
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-04-26 19:24:58 +0900

    Fix : makemigrations error

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db       import models
 from products.models import Product
 
 
+
 class User(models.Model):
     email        = models.CharField(max_length=100,unique=True)
     password     = models.CharField(max_length=100)
