c2fca858a6f944e84694f11ea52da892a4cba22c
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-04-27 15:43:08 +0900

    Add: Status in orders.models

--- orders/models.py
+++ orders/models.py
@@ -2,12 +2,18 @@ from django.db       import models
 from products.models import Product
 from users.models    import User
 
+class Status(models.Model):
+    status = models.CharField(max_length=30)
+
+    class Meta:
+        db_talbe = 'statuses'
+
 class Order(models.Model):
     user     = models.ForeignKey(User,    on_delete=models.CASCADE)
     product  = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.IntegerField()
     price    = models.DecimalField(max_digits=10, decimal_places=2)
-    status   = models.CharField(max_length=30)
+    status   = models.ForeignKey(Status)
 
     class Meta:
         db_table = 'orders'
