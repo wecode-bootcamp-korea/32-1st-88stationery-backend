39aa0169c57ea86478945d3bc297c6c085261f8f
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-04-27 15:45:49 +0900

    Edit : Fix typo

--- orders/models.py
+++ orders/models.py
@@ -6,14 +6,14 @@ class Status(models.Model):
     status = models.CharField(max_length=30)
 
     class Meta:
-        db_talbe = 'statuses'
+        db_table = 'statuses'
 
 class Order(models.Model):
     user     = models.ForeignKey(User,    on_delete=models.CASCADE)
     product  = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.IntegerField()
     price    = models.DecimalField(max_digits=10, decimal_places=2)
-    status   = models.ForeignKey(Status)
+    status   = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'orders'
