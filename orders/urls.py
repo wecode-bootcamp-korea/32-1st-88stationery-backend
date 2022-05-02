cc268a10df647a9af6b520724f49da33b5aa9661
NamkyuAhn <82425544+NamkyuAhn@users.noreply.github.com>
2022-05-02 22:10:01 +0900

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -3,8 +3,5 @@ from orders.views import CartDeleteView, OrderView, OrderCreateView , CartView, 
 
 urlpatterns = [
     path('', OrderView.as_view()),
-    path('/create', OrderCreateView.as_view()),
     path('/cart', CartView.as_view()),
-    path('/cart/create', CartCreateView.as_view()),
-    path('/cart/delete', CartDeleteView.as_view())
 ]
