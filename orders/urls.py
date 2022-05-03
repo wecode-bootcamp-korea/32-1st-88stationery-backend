a929107e9b6077ec47cf685968c9c8e9be6a2ac5
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-05-03 13:19:58 +0900

    Edit : orders.urls

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,5 @@ from orders.views import OrderView, CartView
 
 urlpatterns = [
     path('', OrderView.as_view()),
-    path('/cart', CartView.as_view()),
+    path('/carts', CartView.as_view()),
 ]
